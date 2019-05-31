d10bb1c4b456ba553816200db51840f3ea235890
Jen <jenniferanndcb@gmail.com>
2019-05-31 12:46:40 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,7 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+
   def self.all
     @@all
   end
@@ -10,4 +11,62 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new  
+    song.save 
+    song 
+  end
+  
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+    
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+  
+  def self.find_by_name(name)
+    self.all.detect {|song| song.name == name}    
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+  
+  def self.alphabetical 
+    self.all.sort_by {|s| s.name}
+  end
+  
+  def self.new_from_filename(file_name)
+    song_file = file_name.split(/[-.]/).map(&:strip)
+    name = song_file[1]
+    artist_name = song_file[0]
+    
+    song = self.new 
+    song.name = name
+    song.artist_name = artist_name
+    song
+   
+  end
+    
+    
+  def self.create_from_filename(file_name)
+    song_file = file_name.split(/[-.]/).map(&:strip)
+    name = song_file[1]
+    artist_name = song_file[0]
+    
+    song = self.new 
+    song.name = name
+    song.artist_name = artist_name
+    
+    song.save 
+  end
+  
+  def self.destroy_all
+    self.all.clear 
+  end
 end
