c6175a84be3ca64acc66b5af6acc0ac6930a4a1d
Kristin Kraemer <kristinhannahk@gmail.com>
2019-06-07 00:43:44 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -7,9 +7,9 @@ class Song
   @@all = []
 
   def self.create
-    song = Song.new
-    @@all << song
-    return song
+    song = self.new
+    song.save
+    song
   end
   
   def self.new_by_name(name)
@@ -19,25 +19,22 @@ class Song
   end 
   
   def self.create_by_name(name)
-   song = self.new 
-   song.name = name 
-    @@all << song
-    # is there a way not to repeat this? I tried using #new_by_name but it wasn't working right
-    song
+    song = self.create 
+    song.name = name 
+    song 
   end 
   
   def self.find_by_name(name)
-    @@all.find {|p| p.name == name}
+    self.all.find {|p| p.name == name}
   end 
   
   def self.find_or_create_by_name(name)
-      self.create_by_name(name)
-      self.find_by_name(name)
+      self.find_by_name(name) || self.create_by_name(name)
       #why does this work? why doesn't it have to check if the song is already there?
   end 
   
   def self.alphabetical
-    @@all.sort_by {|song| song.name }
+    all.sort_by {|song| song.name }
   end 
   
   def self.new_from_filename(file_name)
@@ -50,21 +47,14 @@ class Song
   end 
 
   def self.create_from_filename(file_name)
-     song = self.new
-     parts = file_name.split("-")
-     song.artist_name = parts[0][0...-1]
-     title = parts[1]
-     song.name = title[1...-4]
-     @@all << song 
+     song = self.new_from_filename(file_name)
+     song.save
+      song 
   end 
   
   def self.destroy_all
-    @@all = []
+    self.all.clear
   end 
-
-  def song 
-    @song 
-  end
   
   def self.all
     @@all
