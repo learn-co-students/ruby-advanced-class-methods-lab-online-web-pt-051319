914f5178f44bd3d243aa7efbd107e3cda177f7e9
Annia Forney <anniaforney@gmail.com>
2019-06-14 00:15:53 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -11,15 +11,25 @@ class Song
   end
 
   def self.create
+    song = Song.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(name)
     
   end
-
-  def self.new_by_name
-  end
-
-  def self.create_by_name
-  end
-
-
 
 end
